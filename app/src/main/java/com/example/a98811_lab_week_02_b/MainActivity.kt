9ff0dbaaa8462e223e2815f27a82d541f897966b
kkaveman <kevinanatta1@gmail.com>
2025-09-05 15:38:44 +0700

    Commit No.3 - Fix application crash when user input wrong color code [intent]

--- app/src/main/java/com/example/a98811_lab_week_02_b/MainActivity.kt
+++ app/src/main/java/com/example/a98811_lab_week_02_b/MainActivity.kt
@@ -9,15 +9,25 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
 import android.widget.Toast
 import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     companion object{
         private const val COLOR_KEY = "COLOR_KEY"
+        private const val ERROR_KEY = "ERROR_KEY"
     }
 
     private val submitButton: Button
         get() = findViewById(R.id.submit_button)
 
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        activityResult ->
+        val data = activityResult.data
+        val error = data?.getBooleanExtra(ERROR_KEY,false)
+        if(error==true){
+            Toast.makeText(this,getString(R.string.color_code_input_invalid),Toast.LENGTH_LONG).show()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -33,7 +43,8 @@ class MainActivity : AppCompatActivity() {
                 else{
                     val ResultIntent = Intent(this, result::class.java)
                     ResultIntent.putExtra(COLOR_KEY,colorCode)
-                    startActivity(ResultIntent)
+                    //startActivity(ResultIntent)
+                    startForResult.launch(ResultIntent)
                 }
             }
             else{
--- app/src/main/java/com/example/a98811_lab_week_02_b/result.kt
+++ app/src/main/java/com/example/a98811_lab_week_02_b/result.kt
@@ -9,11 +9,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.graphics.Color
 import android.widget.TextView
+import android.content.Intent
+import android.app.Activity
 
 class result : AppCompatActivity() {
 
     companion object{
         private const val COLOR_KEY = "COLOR_KEY"
+        private const val ERROR_KEY = "ERROR_KEY"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +28,18 @@ class result : AppCompatActivity() {
             val colorCode = intent.getStringExtra(COLOR_KEY)
 
             val backgroundscreen = findViewById<ConstraintLayout>(R.id.background_screen)
-            backgroundscreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+
+            try{
+                backgroundscreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+            }
+            catch (ex: IllegalArgumentException){
+                Intent().let{
+                    errorIntent ->
+                    errorIntent.putExtra(ERROR_KEY,true)
+                    setResult(Activity.RESULT_OK,errorIntent)
+                    finish()
+                }
+            }
             val resultMessage = findViewById<TextView>(R.id.color_code_result_message)
             resultMessage.text = getString(R.string.color_code_result_message,colorCode?.uppercase())
         }
